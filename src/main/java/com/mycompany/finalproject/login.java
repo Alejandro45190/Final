0e03d7a962985f3224e12efb67ecdac82f85726f
Alejandro45190 <53579036+Alejandro45190@users.noreply.github.com>
2020-12-01 22:25:44 -0600

    corrected the login class since I set the username and password and no need for setting constructor or setters and getters

--- src/main/java/com/mycompany/finalproject/login.java
+++ src/main/java/com/mycompany/finalproject/login.java
@@ -4,31 +4,6 @@ import java.util.Scanner;
 
 public class login {
 
-    private static String username;
-    private static String password;
-
-    public login(String username, String password)
-    {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     static void login() {
         int redo = 0;
         Scanner sc = new Scanner(System.in);
